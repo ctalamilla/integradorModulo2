092d7a9ca3534767cfb31419fb007d7823947899
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-01-16 17:34:42 -0300

    estapa 3 completada

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* Etapa 1 
+/* Etapa 1 y 2
 
 En esta primera etapa, necesitamos requerir tu módulo autos que se encuentra en la misma carpeta del archivo donde estás trabajando.
 
@@ -8,21 +8,50 @@ Por último, nuestro objeto literal debe tener un atributo llamado autos que con
 
 */
 
+/* let autos = require("./autos");
+
+let concesionaria = {
+    autos: autos,
+    
+    buscarAuto: function(patente){
+        let resultado = concesionaria.autos.filter(auto => auto.patente === patente);
+        return resultado.length == !0 ? resultado[0] : null // ternario
+    },
+
+
+}
+
+console.log(concesionaria.buscarAuto("APL123")) */
+
+/* Etapa 3
+
+Ahora, María les pide que agreguen la funcionalidad de venderAuto que recibe la patente y, en caso de encontrar al automóvil, le asigna el estado de vendido.
+
+¿Cómo lo resuelven?
+
+Recordatorio: Para comenzar tenés que agregar el código que escribiste en el ejercicio anterior. Para resolver esta nueva funcionalidad, tendrás que utilizar la función buscarAuto.
+
+*/
+
 let autos = require("./autos");
 
 let concesionaria = {
     autos: autos,
+    
     buscarAuto: function(patente){
         let resultado = concesionaria.autos.filter(auto => auto.patente === patente);
-        return resultado.length == !0 ? resultado[0] : null // ternario
-/*         if(resultado.length !=0){ 
-            return resultado
-        } else { 
-                return null 
-            } */
-    }    
-}
+        return resultado.length == !0 ? resultado[0] : null; // ternario
+    },
 
+    venderAuto: function(patente){
+         let autoBuscado = this.buscarAuto(patente);
+         autoBuscado.vendido = true;
+    },
+
+    }
+
+concesionaria.venderAuto("APL123")
 console.log(concesionaria.buscarAuto("APL123"))
+
 
 
