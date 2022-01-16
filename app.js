e6b6266f02319c8e2ac16c96e62e7d5afb0f4ee6
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-01-16 14:52:55 -0300

    Primeros objetos literales corregidos

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ let autos = [
         precio: 150000,
         km: 200,
         color: "Azul",
-        coutas: "disponible en 12 coutas",
+        cuotas: 12,
         anio: 2019,
         patente: "APL123",
-        vendido: false        
+         vendido: false        
     },
     {
         marca: "Toyota",
@@ -25,10 +25,10 @@ let autos = [
         precio: 100000,
         km: 0,
         color: "Blanco",
-        coutas: "disponible en 14 coutas",
+        cuotas: 14,
         anio: 2019,
         patente: "JJK116",
         vendido: false  
     }
-] 
+]
 marca, modelo, precio, km, color, cuotas, anio, patente, vendido.
